73e2a154cbbe8306387c98e09559cfc17186a879
KP <88709841+kruno-peter@users.noreply.github.com>
2021-08-16 11:13:19 +0200

    Added empty_htmlized.js
    Added empty_htmlized.js - an example - empty_htmlized.js - calls the htmlize() function without any arguments - returns an (almost) empty Web page

---
+++ empty_htmlized.js
@@ -0,0 +1,16 @@
+// empty_htmlized.js - a simple Web server - returns an empty Web page
+const http = require('http');
+const tinyHTMLizer = require('./tinyHTMLizer.js');    // shorter: th
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(tinyHTMLizer.htmlize());    // htmlize! (without any args)
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
