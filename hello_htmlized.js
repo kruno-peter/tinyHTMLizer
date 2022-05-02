00de39fef75a7570131fc987aa618b696dca2442
KP <88709841+kruno-peter@users.noreply.github.com>
2022-05-02 13:10:58 +0200

    Update hello_htmlized.js
    Change: the shows the server response time and tests whether JavaScript is enabled

--- hello_htmlized.js
+++ hello_htmlized.js
@@ -1,4 +1,5 @@
-// hello_htmlized.js - a simple HTMLized Web app
+// hello_htmlized.js - a simple HTMLized Web app:
+// it shows the server response time and tests whether JavaScript is enabled
 const http = require('http');
 const tinyHTMLizer = require('./tinyHTMLizer.js');
 
@@ -6,10 +7,10 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const content = '<h1>Hi!</h1>The response time: <em>' + new Date() + '</em>';
+  const content = '<h1 id="p1">JS disabled</h1><p>The response time: ' + new Date() + '</p>';
   const title = 'Backend and Frontend Logic';
-  const style = 'html { margin: 3\%; color: blue; }';    // backslash%
-  const script = 'alert("A random number: " + Math.floor(Math.random() * 10));';
+  const style = 'html { margin: 3\%; color: blue; }';    // watch out! backslash%
+  const script = 'window.onload = () => document.getElementById("p1").innerHTML = "JS enabled";';
   const webPage = tinyHTMLizer.htmlize(content, title, style, script);
 
   res.statusCode = 200;
@@ -20,4 +21,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
