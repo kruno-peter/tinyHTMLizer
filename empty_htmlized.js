837d92eaea3d436ee19b7720b1c3cb8e231f7534
KP <88709841+kruno-peter@users.noreply.github.com>
2022-05-16 13:35:54 +0200

    Update empty_htmlized.js
    Removed the .js extension

--- empty_htmlized.js
+++ empty_htmlized.js
@@ -1,6 +1,6 @@
 // empty_htmlized.js - a simple Web server - returns an empty Web page
 const http = require('http');
-const tinyHTMLizer = require('./tinyHTMLizer.js');    // shorter: th
+const tinyHTMLizer = require('./tinyHTMLizer');    // shorter: th
 
 const hostname = '127.0.0.1';
 const port = 3000;
